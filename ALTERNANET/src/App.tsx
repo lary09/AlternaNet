fd5b0abbf6a258f98c3b6ed9416db12a8a68f60e
janCalos <jancarlosjam012@gmail.com>
2024-02-28 00:16:23 -0400

    Creation_front-end

---
+++ ALTERNANET/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <div className='formulario'>
+      <form  action="/">
+        <label htmlFor="campo"><img src="C:\Users\Lenovo\Desktop\Proyecto-Final\AlternaNet\ALTERNANET\src\img\Logo-Proyecto final.png" alt="logo_Nombre" />Nombre:</label><input type ='text'/>
+        <label htmlFor="campo">Contraseña:</label><input type ='password'/>
+        <input type='submit'></input>
+      </form>
+    </div> 
+    
+  )
+}
+
+export default App
